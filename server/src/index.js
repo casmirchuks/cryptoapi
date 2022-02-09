f0ee74300c057c7a4d5481f316dff8a36b07a649
casmir <casmir.chukwuma@sovtech.com>
2022-02-09 10:29:43 +0200

    scope: appended gitignore file

--- server/src/index.js
+++ server/src/index.js
@@ -5,10 +5,11 @@ const axios = require('axios');
 const resolvers = {
     Query: {
         allCoins: async () => {
-            const allCoinList = [];
             const response = await axios.get('https://api.coinlore.net/api/tickers/')
-            // console.log(response.data);
-            response.data.map(obj => {allCoinList.push(obj)})
+            const allCoinList = [];
+            responseData = JSON.stringify(response);
+            console.log(responseData);
+            responseData.map((eachCoin) => allCoinList.push(eachCoin))
             return allCoinList;
         }
     }
